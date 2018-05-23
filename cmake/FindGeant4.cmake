ebea7477172abc6ddf3709742dd603dd3d4a5c6f
Sveta Shasharina <sveta@txcorp.com>
2018-05-23 11:41:38 -0600

    Chaning looking for cmake of Geant4.

--- cmake/FindGeant4.cmake
+++ cmake/FindGeant4.cmake
@@ -6,17 +6,5 @@
 #  Geant4_INCLUDE_DIRS - Geant4 include directories
 #  Geant4_LIBRARIES - In case they are ever needed
 
-# Try to find Geant4Config.cmake
-file(GLOB SEARCH_DIRS "${GEANT4_DIR}/lib*/Geant4-*")
-string(REPLACE "\n" ";" SEARCH_DIRS ${SEARCH_DIRS})
-find_path(GEANT4_CMAKE_CONFIG
-          NAMES Geant4Config.cmake
-          PATHS ${SEARCH_DIRS}
-          NO_DEFAULT_PATH)
-if (${GEANT4_CMAKE_CONFIG} MATCHES GEANT4_CMAKE_CONFIG-NOTFOUND)
-  message(FATAL_ERROR "Geant4Config.cmake not found. Make sure to set -DGEANT4_DIR when running CMake.")
-endif (${GEANT4_CMAKE_CONFIG} MATCHES GEANT4_CMAKE_CONFIG-NOTFOUND)
-message(STATUS "Found Geant4Config.cmake at ${GEANT4_CMAKE_CONFIG}")
-
 # Geant4 CMake config
 include(${GEANT4_CMAKE_CONFIG}/Geant4Config.cmake)
